8646bcdb51b0d034e10ac9fa920eff4ab9abfbff
petromoldovan <petro.moldovan@simplesurance.de>
2017-07-30 13:53:31 +0200

    f

--- 14_func/defer/main.go
+++ 14_func/defer/main.go
@@ -1,1 +1,16 @@
-package defer
+package main
+
+import "fmt"
+
+func hello() {
+	fmt.Println("hello ")
+}
+
+func world() {
+	fmt.Println("world")
+}
+
+func main() {
+	defer world()
+	hello()
+}
--- 14_func/defer/openFile/main.go
+++ 14_func/defer/openFile/main.go
@@ -1,1 +1,14 @@
-package openFile
+package main
+
+import (
+	"os"
+)
+
+func main() {
+	src, err := os.Open("src.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer src.Close()
+}
