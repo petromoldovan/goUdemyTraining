8816dc6dd4dc5ba9f11ad5ca858f503042d91373
petromoldovan <petro.moldovan@simplesurance.de>
2017-08-05 14:38:51 +0200

    show all and by id methods

--- REST/main.go
+++ REST/main.go
@@ -6,7 +6,6 @@ import (
 	"database/sql"
 	_ "github.com/lib/pq"
 	"encoding/json"
-	"os"
 )
 
 var db *sql.DB
@@ -35,12 +34,16 @@ type User struct {
 
 func main() {
 	http.HandleFunc("/users/show", usersShow)
+	http.HandleFunc("/users", userShowByID)
 	http.ListenAndServe(":8080", nil)
 }
 
+//
+// get all users
+//
 func usersShow(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
-		http.Error(w, http.StatusText(511), http.StatusMethodNotAllowed)
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
 		return
 	}
 
@@ -70,8 +73,45 @@ func usersShow(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	json.NewEncoder(os.Stdout).Encode(users)
+	marshaledUsers, err := json.Marshal(users)
+
+	//set cors
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Server", "User Service")
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(marshaledUsers)
 }
 
+//
+// get specific user by id
+//
+func userShowByID(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(511), http.StatusMethodNotAllowed)
+		return
+	}
 
+	//reading query param
+	id := r.FormValue("id")
+	if id == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
 
+	row := db.QueryRow("SELECT * FROM users WHERE id=$1", id)
+
+	user := User{}
+
+	err := row.Scan(&user.ID, &user.FirstName, &user.Surname, &user.PhoneNumber, &user.Email, &user.isActive)
+	switch {
+		case err == sql.ErrNoRows:
+			http.NotFound(w, r)
+			return
+		case err != nil:
+			http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+			return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
